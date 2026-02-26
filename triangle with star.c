8633609c9a2b406c8023e009c1eac43b91782c2c
Mohammad Kaif Raj <91706309+MasterKai19@users.noreply.github.com>
2026-02-26 11:34:36 +0600

    Add files via upload

---
+++ triangle with star.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,n;
+    printf ("give value of n:  ");
+    scanf("%d", &n);
+
+    for( i=n; i>=1; i--)
+    {
+
+        for (j=1; j<=i; j++)
+        {
+
+            printf("*");
+        }
+        printf("\n");
+
+    }
+return 0;
+
+
+}
